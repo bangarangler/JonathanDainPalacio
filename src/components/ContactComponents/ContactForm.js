64de91b41fae608f96ea2caff4c906755822434e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-06 21:00:55 -0400

    Added Some styling to new form

--- src/components/ContactComponents/ContactForm.js
+++ src/components/ContactComponents/ContactForm.js
@@ -1,37 +1,52 @@
 import React from "react"
+import styles from "./ContactForm.module.less"
+import email from "../../images/email.svg"
 
 const ContactForm = props => {
   return (
-    <form
-      action="#"
-      method="post"
-      name="contact"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6" />
-      </div>
-      <ul>
-        <li>
-          <input type="submit" value="Send Message" />
-        </li>
-        <li>
-          <input type="reset" value="Clear" />
-        </li>
-      </ul>
-    </form>
+    <div className={styles.wrapper}>
+      <img src={email} alt="email SVG" />
+      <form
+        action="#"
+        method="post"
+        name="contact"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" placeholder="Name..." />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email Address..."
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            rows="6"
+            placeholder="Your Message!"
+          />
+        </div>
+        <ul>
+          <li>
+            <input type="submit" value="Send Message" />
+          </li>
+          <li>
+            <input type="reset" value="Clear" />
+          </li>
+        </ul>
+      </form>
+    </div>
   )
 }
 
