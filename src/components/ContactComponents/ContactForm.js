1ff441a84dc54ee19f3f84cfdc909b51e2da5f27
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-07 18:05:47 -0400

    Deleted yarn.lock file and fixed lots of probs
    hate yarn. never use it. forgot to delete yarn.lock. most of queries
    started working again. still trying to figure out safari mobile bug.
    yuck

--- src/components/ContactComponents/ContactForm.js
+++ src/components/ContactComponents/ContactForm.js
@@ -1,52 +1,82 @@
 import React from "react"
 import styles from "./ContactForm.module.less"
-import email from "../../images/email.svg"
+//import email from "../../images/email.svg"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const QUERY = graphql`
+  {
+    image: file(relativePath: { eq: "email.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const ContactForm = props => {
   return (
-    <div className={styles.wrapper}>
-      <img src={email} alt="email SVG" />
-      <form
-        action="#"
-        method="post"
-        name="contact"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" placeholder="Name..." />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email Address..."
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            rows="6"
-            placeholder="Your Message!"
-          />
-        </div>
-        <ul>
-          <li>
-            <input type="submit" value="Send Message" />
-          </li>
-          <li>
-            <input type="reset" value="Clear" />
-          </li>
-        </ul>
-      </form>
-    </div>
+    <StaticQuery
+      query={QUERY}
+      render={data => {
+        return (
+          <div className={styles.wrapper}>
+            <Img
+              fluid={data.image.childImageSharp.fluid}
+              alt="email SVG"
+              className={styles.img}
+            />
+            <form
+              action="#"
+              method="post"
+              name="contact"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name..."
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address..."
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="6"
+                  placeholder="Your Message!"
+                />
+              </div>
+              <ul>
+                <li>
+                  <input type="submit" value="Send Message" />
+                </li>
+                <li>
+                  <input type="reset" value="Clear" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        )
+      }}
+    />
   )
 }
 
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,34 +1,45 @@
 import React from "react"
 import styles from "./Resume.module.less"
-//import { StaticQuery, graphql } from "gatsby"
-//import Img from "gatsby-image"
-import img from "../../images/resume.png"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+//import img from "../../images/resume.png"
 
-//const QUERY = graphql`
-//{
-//image: file(relativePath: { eq: "resume.jpg" }) {
-//childImageSharp {
-//fluid {
-//...GatsbyImageSharpFluid_tracedSVG
-//}
-//}
-//}
-//}
-//`
+const QUERY = graphql`
+  {
+    image: file(relativePath: { eq: "resume.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 const Resume = props => {
   const [resume, setResume] = React.useState(false)
   function showResume() {
     setResume(!resume)
   }
   return (
-    <section className={styles.resumeWrapper}>
-      <h3 className={styles.resumeHeading} onClick={() => showResume()}>
-        Jonathan's Resume
-      </h3>
-      {resume && (
-        <img className={styles.resumeImg} src={img} alt="Jons Resume" />
-      )}
-    </section>
+    <StaticQuery
+      query={QUERY}
+      render={data => {
+        return (
+          <section className={styles.resumeWrapper}>
+            <h3 className={styles.resumeHeading} onClick={() => showResume()}>
+              Jonathan's Resume
+            </h3>
+            {resume && (
+              <Img
+                className={styles.resumeImg}
+                fluid={data.image.childImageSharp.fluid}
+                alt="Jons Resume"
+              />
+            )}
+          </section>
+        )
+      }}
+    />
   )
 }
 
